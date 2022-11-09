508935c1fa97171663893bc63a3b9f035bbc72e3
fkbeys <fkbeys@gmail.com>
2022-11-10 02:52:43 +0400

    ok

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -21,14 +21,14 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseOcelot().Wait();
 
--- Services/Contact/Contact.Api/Program.cs
+++ Services/Contact/Contact.Api/Program.cs
@@ -21,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
